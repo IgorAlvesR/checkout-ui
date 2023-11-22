90d6e635ccf15e3cc19e878efd7527602bb8ff61
Igor <igor@cincatarina.sc.gov.br>
2023-11-22 15:02:07 -0300

    test: cria testes para a ação do botão.

--- src/components/FormCheckout.spec.tsx
+++ src/components/FormCheckout.spec.tsx
@@ -3,6 +3,14 @@ import { render, screen } from '@testing-library/react'
 import FormCheckout from './FormCheckout'
 import userEvent from '@testing-library/user-event'
 
+const sleep = (time: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, time)
+  })
+}
+
 describe('FormCheckout component', () => {
   render(<FormCheckout />)
 
@@ -31,5 +39,32 @@ describe('FormCheckout component', () => {
     const error = screen.queryByTestId('error-card-number')
 
     expect(error).toBeNull()
+  })
+
+  it(`deve verificar se o botão do formulário é desabilitado ao preencher os 
+  campos e quando clicado se os campos voltam a ser vazios`, async () => {
+    const button = screen.getByRole('button')
+
+    expect(button).toHaveProperty('disabled', true)
+
+    const inputCardNumber = screen.getByPlaceholderText(
+      '9999 9999 9999 9999',
+    ) as HTMLInputElement
+    const inputCardExpirationDate = screen.getByPlaceholderText(
+      '10/23',
+    ) as HTMLInputElement
+
+    await userEvent.click(inputCardNumber)
+    await userEvent.keyboard('5435 3761 4337 8337')
+    await userEvent.click(inputCardExpirationDate)
+    await userEvent.keyboard('10/24')
+
+    expect(button).toHaveProperty('disabled', false)
+    await userEvent.click(button)
+
+    await sleep(200)
+
+    expect(inputCardNumber.value).toBe('')
+    expect(inputCardExpirationDate.value).toBe('')
   })
 })
